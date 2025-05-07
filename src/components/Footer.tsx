a40f5e1bda10dc0154f9070a6311ab7059384962
Abhishek Pundhir <144231613+abhishekpundhir@users.noreply.github.com>
2025-05-07 12:27:19 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,13 +33,14 @@ const Footer = () => {
           
           <div className="mt-8 md:mt-0">
             <p className="text-center text-sm text-vibespace-light-text md:text-right">
-              &copy; {currentYear} VibeSpace. All rights reserved.
+              &copy; 2025 VibeSpace. All rights reserved.
             </p>
           </div>
         </div>
         
-        <div className="mt-8 border-t border-vibespace-border pt-8 md:flex md:items-center md:justify-center">
-          <div className="flex space-x-8 justify-center md:justify-start md:order-2">
+        <div className="mt-8 border-t border-vibespace-border pt-8 md:flex md:items-center md:justify-between">
+         
+          <div className="flex space-x-8 justify-start md:justify-start md:order-2">
             <Link to="https://github.com/abhishekpundhir" className="text-vibespace-light-text hover:text-vibespace-light-purple flex flex-col items-center gap-1">
               <Code className="h-5 w-5" />
               <span className="text-xs">Developers</span>
@@ -57,9 +58,10 @@ const Footer = () => {
               <span className="text-xs">Careers</span>
             </Link>
           </div>
-        </div>
+        
 
-                <div className=" flex  pt-5 space-x-8 justify-start md:justify-start md:order-2">
+
+        <div className=" flex  pt-5 space-x-8 justify-start md:justify-start md:order-2">
           <span className="flex items-center justify-center h-14 w-14 rounded-md  p-2 shadow-lg tech-icon">
               <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white">
                 <path fill="#FFFFFF" d="M70,30 L130,30 L160,80 L100,170 L40,80 Z" />
@@ -69,9 +71,10 @@ const Footer = () => {
                 <path d="M65,85 L80,65 M135,85 L120,65" stroke="#FFFFFF" strokeWidth="4" />
               </svg>
             </span>
-            <b className="flex justify-center items-center">Developed By Team VibeSpace</b>
+            <b className="flex items-center">Developed By Team VibeSpace</b>
           </div>
         </div>
+
       </div>
     </footer>
   );
